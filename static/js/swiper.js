55da55d650bd74691f93ff554bd3aa70cbd137a6
Antonio <antonio@AntoniodeMacBook-Air.local>
2024-05-27 00:48:51 +0800

    fix: 🐛 swiper pagination show bug

--- static/js/swiper.js
+++ static/js/swiper.js
@@ -1,19 +1,20 @@
 var swiper = new Swiper(".article-swiper-container", {
   slidesPerView: 3,
   direction: 'vertical',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    renderBullet: function (index, className) {
-      console.log('className', className)
-      return '<span class="' + className + '">' + "</span>";
-    },
-  },
   breakpoints: {
     993: {
       slidesPerView: 3,
       spaceBetween: 24,
       direction: 'horizontal',
+      pagination: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          console.log('className', className)
+          return '<span class="' + className + '">' + "</span>";
+        }
+      }
     }
   },
 });
