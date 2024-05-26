19299d873aa39c761dedad6b436886c332260c1a
Antonio <antonio@AntoniodeMacBook-Air.local>
2024-05-27 00:36:11 +0800

    feat: 🎸 index.html RWD complete

--- static/js/swiper.js
+++ static/js/swiper.js
@@ -1,30 +1,19 @@
 var swiper = new Swiper(".article-swiper-container", {
   slidesPerView: 3,
-  spaceBetween: 24,
+  direction: 'vertical',
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     renderBullet: function (index, className) {
+      console.log('className', className)
       return '<span class="' + className + '">' + "</span>";
     },
   },
   breakpoints: {
-    376: {
+    993: {
       slidesPerView: 3,
       spaceBetween: 24,
-      direction: 'horizontal', // 恢复水平布局
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + "</span>";
-        },
-      },
-    },
-    375: {
-      slidesPerView: 3,
-      direction: 'vertical',
-      pagination: false,
-    },
+      direction: 'horizontal',
+    }
   },
 });
