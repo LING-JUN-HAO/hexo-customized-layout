19c9f264409270e9a8c9988321aa4311ae3b5bab
Antonio <antonio@AntoniodeMacBook-Air.local>
2024-05-27 21:32:47 +0800

    feat: 🎸 nav active overlay

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,12 @@
-$('.navbar-btn ').click(function() {
-    $('.navbar-btn').toggleClass('active');
-    $('.menu').toggleClass('close');
-    $('.closure').toggleClass('close');
-    $('.nav-list').toggleClass('active');
-  })
+$('.navbar-btn ').click(function () {
+  $('.navbar-btn').toggleClass('active');
+  $('.menu').toggleClass('close');
+  $('.closure').toggleClass('close');
+  $('.nav-list').toggleClass('active');
+
+  if ($('.nav-list').hasClass('active')) {
+    $('body').addClass('overlay');
+  } else {
+    $('body').removeClass('overlay');
+  }
+})
